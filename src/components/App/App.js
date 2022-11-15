69a618ac09cfa1ba868351cb1dc37966e2bb0f66
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-11-15 09:49:15 -0700

    complete Iteration 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from "react";
+import "./App.css";
+import { getUrls } from "../../apiCalls";
+import UrlContainer from "../UrlContainer/UrlContainer";
+import UrlForm from "../UrlForm/UrlForm";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
-    }
+      urls: [],
+      errors: "",
+    };
   }
 
   componentDidMount() {
     getUrls()
-    .then(data => {
-      console.log(data)
-      this.setState({urls: data.urls})
-    })
+      .then((data) => {
+        console.log(data);
+        this.setState({ urls: data.urls });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ errors: err });
+      });
   }
 
   render() {
@@ -28,7 +33,7 @@ export class App extends Component {
           <UrlForm />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={this.state.urls} />
       </main>
     );
   }
